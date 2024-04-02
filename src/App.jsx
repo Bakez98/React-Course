0301629bc3946e214168671feb9f4d91ba800ae1
Bakez98 <omar.bakez@hotmail.com>
2024-04-02 20:48:14 +0300

    #46 Best Practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import reactImg from "../src/assets/react-core-concepts.png";
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const descreption = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {descreption} Fundamental React concepts you will need for almost any
-        app you are going to build!
-      </p>
-    </header>
-  );
-}
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        <h2>Time to get started!</h2>
+      <main id="core-concepts">
+        <seciton>
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept, i) => {
+              return (
+                <CoreConcept
+                  key={i}
+                  title={concept.title}
+                  descreption={concept.description}
+                  img={concept.image}
+                />
+              );
+            })}
+          </ul>
+        </seciton>
       </main>
     </div>
   );
---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,9 @@
+export default function CoreConcept({ title, descreption, img }) {
+  return (
+    <li>
+      <img src={img} alt={title} />
+      <h3>{title}</h3>
+      <p>{descreption}</p>
+    </li>
+  );
+}
