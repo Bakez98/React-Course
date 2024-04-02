87672ab60059ff8733aba741a28acf051996e1b1
Bakez98 <omar.bakez@hotmail.com>
2024-04-02 18:19:15 +0300

    outputting dynamic values

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import React from "react";
+import reactImg from "../src/assets/react-core-concepts.png";
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
-function MainGoal() {
-  return <p>My main goal: Learn React from the ground up.</p>;
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const descreption = reactDescriptions[genRandomInt(2)];
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {descreption} Fundamental React concepts you will need for almost any
+        app you are going to build!
+      </p>
+    </header>
+  );
 }
 
 function App() {
   return (
-    <div id="app">
-      <h1>Time to Practice!</h1>
-      <p>
-        Build a <code>&lt;MainGoal&gt;</code> component and insert it below this
-        text.
-      </p>
-      <p>
-        Your <code>&lt;MainGoal&gt;</code> component should simply output some
-        text that describes your main course goal (e.g., &quot;My main goal:
-        Learn React in great detail&quot;).
-      </p>
-      <p>
-        <strong>Important:</strong> You custom component must contain the text
-        &quot;My main goal:&quot;
-      </p>
-      <p>
-        <strong>Also important:</strong> For the automatic checks to succeed,
-        you <strong>must export</strong> your custom component function! Not as
-        a default but simply by adding the <code>export</code> keyword in front
-        of your function (e.g., <code>export function YOUR_COMPONENT_NAME</code>
-        ).
-      </p>
-      <MainGoal />
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
     </div>
   );
 }
