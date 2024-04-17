eaee924023cbc82b89ded197a2c5228bc4f93c41
Bakez98 <omar.bakez@hotmail.com>
2024-04-17 19:18:18 +0300

    pushing section 3 video #55

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,27 @@ import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcepts/CoreConcept";
 import TabButton from "./components/TabButton";
 import "./index.css";
+import { EXAMPLES } from "./data";
+import { useState } from "react";
 
 function App() {
-  function handleSelect() {
-    console.log("hello World!");
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please Select a topic</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].descreption}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
   return (
     <div>
@@ -30,13 +47,26 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={handleSelect} label="Components" />
-            <TabButton onClick={handleSelect} label="JSX" />
-            <TabButton onClick={handleSelect} label="Props" />
-            <TabButton onClick={handleSelect} label="States" />
+            <TabButton
+              onSelect={() => handleSelect("components")}
+              label="Components"
+            />
+            <TabButton onSelect={() => handleSelect("jsx")} label="JSX" />
+            <TabButton onSelect={() => handleSelect("props")} label="Props" />
+            <TabButton onSelect={() => handleSelect("state")} label="States" />
           </menu>
-          Dynamic Content
+          {/* {!selectedTopic && <p>Please Select a topic</p>} */}
+          {/* {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].descreption}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )} */}
         </section>
+        {tabContent}
       </main>
     </div>
   );
